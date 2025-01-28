1a00ec2f85978df43464d90aa06e10359a59d640
whatsappcrashm <rirani293@gmail.com>
2025-01-28 17:46:56 +0600

    Add files via upload

---
+++ .github/workflows/config.js
@@ -0,0 +1,6 @@
+const global = {
+    owner: [6142885267], // ganti jadi id mu
+    botToken: "7725260979:AAF9pls5ef2mKp0eZu-djXeFnnwC5XCRyL8", //isi make token bot mu
+}
+
+module.exports = global;
